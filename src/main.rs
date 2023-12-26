41f2b45e9a67bfac99b562d77b9f10b57287ca67
Civitasv <hscivitasv@gmail.com>
2023-12-26 20:44:50 +0800

    playing with cross-term

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use crossterm::{
+    execute,
+    style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor},
+};
+use std::io::stdout;
+
+fn main() -> std::io::Result<()> {
+    execute!(
+        stdout(),
+        SetForegroundColor(Color::Blue),
+        SetBackgroundColor(Color::Red),
+        Print("Styled text here."),
+        ResetColor
+    )?;
+
+    Ok(())
 }
